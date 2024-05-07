c3015599a514ba26686dfaa7bd5134982082f44a
Kajal-rajodiya <kajalrajodiya1@gmail.com>
2024-05-07 12:30:07 +0530

    commit

--- JS/LinkedList.java
+++ JS/LinkedList.java
@@ -1,6 +1,32 @@
 import java.util.Scanner;
 
 public class LinkedList {
+    public class SinglyList{
+        static class Node
+        {
+            private int data;
+            private Node next;
+            public Node()
+            {
+                data=0;
+                next=null;
+            }
+            public Node(int val)
+            {
+                data= val;
+                next= null;
+            }
+
+        }
+    
+        private Node head;
+        public SinglyList()
+        {
+            head=null;
+        }
+    }
+
+
     public static void main(String args[])
     {
         int choice;
