563f3182b9d52286d63c34fde9d581dcc224b7f0
Kajal-rajodiya <kajalrajodiya1@gmail.com>
2024-05-07 12:15:47 +0530

    commit

--- JS/LinkedList.java
+++ JS/LinkedList.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class LinkedList {
     public static void main(String args[])
     {
-        
+        int choice;
+        Scanner sc= new Scanner(System.in);
+        do{
+
+            choice=sc.nextInt();
+            switch (choice) {
+                case 1:
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+        }
     }
     
 }
